fbd947dabaa16dd150b4aa12747775ca5be30810
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-02-24 23:58:48 +0100

    standard page, lighthouse improvements

--- willys_website/core/static/scripts/main.js
+++ willys_website/core/static/scripts/main.js
@@ -3,6 +3,20 @@
 
     $(function(){
 
+        /************ SW *************/
+
+        if ('serviceWorker' in navigator) {
+          window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/static/scripts/sw.js').then(function(registration) {
+              // Registration was successful
+              console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }).catch(function(err) {
+              // registration failed :(
+              console.log('ServiceWorker registration failed: ', err);
+            });
+          });
+        }
+
         /************ COOKIES *************/
 
         function getCookie(cname) {
