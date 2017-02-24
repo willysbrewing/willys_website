c74b29ab20594bf2360154198b1c97251ca9dde2
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-02-24 22:43:54 +0100

    GTM, seo, icons

--- willys_website/core/static/scripts/main.js
+++ willys_website/core/static/scripts/main.js
@@ -20,7 +20,7 @@
             var d = new Date();
             d.setTime(d.getTime() + (exdays*24*60*60*1000));
             var expires = 'expires='+d.toUTCString();
-            document.cookie = cname + '=' + cvalue + '; ' + expires;
+            document.cookie = cname + '=' + cvalue + '; ' + expires+"; path=/";
         }
 
         (function(){
